fd7915b78f8136ff108ab3a3ca3ec72f0cbbcb20
3zk1m0 <sander.kallio@gmail.com>
2021-12-27 18:16:38 +0200

    Refactor day 25

--- src/2021/day25/index.ts
+++ src/2021/day25/index.ts
@@ -1,77 +1,33 @@
 import { test, readInput, runPart } from '../../utils'
 
-
 const prepareInput = (rawInput: string) => rawInput.split('\n').map(row => row.split(''))
 
-const stepEast = (grid:string[][]) => {
-
-  const newGrid = JSON.parse(JSON.stringify(grid))
-    .map(row => row.fill('.'))
-
-  for (let [y, row] of Object.entries(grid)) {
-    for (let [x, pos] of Object.entries(row)) {
-      if (pos !== '>') {
-        if (newGrid[y][x] !== '>') {
-          newGrid[y][x] = pos
-        }
-        continue
-      }
-      const xD = (+x + 1) % row.length
-      if (grid[y][xD] === '.') {
-        newGrid[y][xD] = '>'
-        continue
-      }
-      newGrid[y][x] = '>'
+const part1 = (map:string[][]) => {
+  let count = 0
+  let changed = true
+  const width = map[0].length
+  const height = map.length
+  while (changed) {
+    changed = false
+    for (let turn of ['>', 'v']) {
+      const newMap = map.map(row => row.map(cell => cell))
+      map.forEach((row, y) =>
+        row.forEach((cell, x) => {
+          const dx = turn === '>' ? (x + 1) % width : x
+          const dy = turn === 'v' ? (y + 1) % height : y
+          if (turn === cell && map[dy][dx] === '.') {
+            newMap[y][x] = '.'
+            newMap[dy][dx] = turn
+            changed = true
+          }
+        })
+      );
+      map = newMap
     }
+    count++
   }
-  return newGrid
-}
 
-const stepSouth = (grid:string[][]) => {
-
-  const newGrid = JSON.parse(JSON.stringify(grid))
-    .map(row => row.fill('.'))
-
-  for (let [y, row] of Object.entries(grid)) {
-    for (let [x, pos] of Object.entries(row)) {
-      if (pos !== 'v') {
-        if (newGrid[y][x] !== 'v') {
-          newGrid[y][x] = pos
-        }
-        continue
-      }
-      const yD = (+y + 1) % grid.length
-      if (grid[yD][x] === '.' ) {
-        newGrid[yD][x] = 'v'
-        continue
-      }
-      newGrid[y][x] = 'v'
-    }
-  }
-  return newGrid
-}
-
-const step = (grid) => {
-  let newGrid = stepEast(grid)
-  newGrid = stepSouth(newGrid)
-  return newGrid
-}
-
-const part1 = (input) => {
-  let grid = JSON.parse(JSON.stringify(input))
-  let newGrid = JSON.parse(JSON.stringify(input))
-
-  let steps = 0
-  while (true) {
-    steps++
-    newGrid = step(grid)
-    if (JSON.stringify(newGrid) === JSON.stringify(grid)) {
-      break
-    }
-    grid = newGrid
-  }
-  
-  return steps
+  return count
 }
 
 
@@ -83,7 +39,7 @@ const part1 = (input) => {
 
 export const main = async (args) => {
   const input: any = prepareInput(await readInput(args))
-  runPart("Part One:", () => part1(input))
+  runPart('Part One:', () => part1(input))
 }
 
  
