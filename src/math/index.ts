a0d52883d74870fc440eb8fb91aeb24b5fb75678
3zk1m0 <sander.kallio@monad.fi>
2023-12-11 08:22:25 +0200

    Day 11 Year 2023

--- src/math/index.ts
+++ src/math/index.ts
@@ -3,3 +3,9 @@ export const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b)
 
 // Least Common Multiple
 export const lcm = (a: number, b: number) => (a * b) / gcd(a, b)
+
+export const manhattanDistance = (a: [number, number], b: [number, number]) => {
+  const [x1, y1] = a
+  const [x2, y2] = b
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2)
+}
